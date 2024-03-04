a99aec0ba447e91693dba54d399827b58c723d3a
herbertfilhik <herbertfilhik@gmail.com>
2024-03-04 10:40:32 -0300

    update security

--- src/main/java/com/management/config/WebSecurityConfig.java
+++ src/main/java/com/management/config/WebSecurityConfig.java
@@ -9,36 +9,44 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-			)
-			.formLogin((form) -> form
-				.loginPage("/login")
-				.permitAll()
-			)
-			.logout((logout) -> logout.permitAll());
+//    @SuppressWarnings("deprecation")
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests()
+//                // Permitir acesso a recursos estáticos sem autenticação
+//                .requestMatchers("/css/**", "/js/**", "/images/**", "/login.css").permitAll()
+//                // Permitir acesso às páginas específicas sem autenticação
+//                .requestMatchers("/", "/home", "/login").permitAll()
+//                // Todas as outras solicitações exigem autenticação
+//                .anyRequest().authenticated()
+//                .and()
+//            .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//            .logout()
+//                .permitAll();
+//
+//        return http.build();
+//    }
 
-		return http.build();
-	}
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
-	}
+        return new InMemoryUserDetailsManager(user);
+    }
 }
