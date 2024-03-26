d39b3bb03f74c65620396fd5ce61851346fab17c
herbertfilhik <herbertfilhik@gmail.com>
2024-03-26 16:49:43 -0300

    update for swagger

--- src/main/java/com/management/config/SecurityConfig.java
+++ src/main/java/com/management/config/SecurityConfig.java
@@ -16,13 +16,14 @@ public class SecurityConfig {
         http
         	.csrf().disable()
             .authorizeRequests()
-            .requestMatchers("/cars").hasRole("ADMIN") // Exemplo: apenas usuários com o papel 'ADMIN' podem acessar
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/v2/api-docs", "/swagger-resources/**", "/webjars/**").permitAll() // Permite o acesso ao Swagger UI e recursos relacionados
+            .requestMatchers("/cars/**").hasRole("ADMIN") // Exemplo: apenas usuários com o papel 'ADMIN' podem acessar "/cars/**"
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // Permite o acesso a recursos estáticos
-            .anyRequest().authenticated()
+            .anyRequest().authenticated() // Todas as outras requisições precisam de autenticação
             .and()
-            .formLogin()
+            .formLogin() // Habilita o formulário de login
             .and()
-            .logout();
+            .logout(); // Habilita o logout
 
         return http.build();
     }
